f6e1437370a51180a27ff2cdfb37826769ed4cea
Kanata Miyahana <afiri.bupa@gmail.com>
2022-06-16 22:02:13 +0900

    update

--- repl/main.go
+++ repl/main.go
@@ -4,47 +4,39 @@ import (
 	"errors"
 	"fmt"
 	"go/ast"
+	"go/constant"
 	"go/parser"
-	"go/token"
-	"strconv"
 )
 
-func eval1pls1(expr *ast.BinaryExpr) (int64, error) {
+func evalBinaryExpr(expr *ast.BinaryExpr) (constant.Value, error) {
+
 	xLit, ok := expr.X.(*ast.BasicLit)
 	if !ok {
-		return 0, errors.New("left operand is not BasicLit")
+		return constant.MakeUnknown(), errors.New("left operand is not BasicLit")
 	}
 
 	yLit, ok := expr.Y.(*ast.BasicLit)
 	if !ok {
-		return 0, errors.New("right operand is not BasicLit")
+		return constant.MakeUnknown(), errors.New("right operand is not BasicLit")
 	}
 
-	if expr.Op != token.ADD {
-		return 0, errors.New("operator is not +")
-	}
+	x := evalBasicLit(xLit)
+	y := evalBasicLit(yLit)
+	return constant.BinaryOp(x, expr.Op, y), nil
+}
 
-	x, err := strconv.ParseInt(xLit.Value, 10, 64)
-	if err != nil {
-		return 0, err
-	}
-
-	y, err := strconv.ParseInt(yLit.Value, 10, 64)
-	if err != nil {
-		return 0, err
-	}
-
-	return x + y, nil
+func evalBasicLit(expr *ast.BasicLit) constant.Value {
+	return constant.MakeFromLiteral(expr.Value, expr.Kind, 0)
 }
 
 func main() {
-	expr, err := parser.ParseExpr("1+1")
+	expr, err := parser.ParseExpr("2*10")
 	if err != nil {
 		panic(err)
 	}
 
 	if be, ok := expr.(*ast.BinaryExpr); ok {
-		if v, err := eval1pls1(be); err == nil {
+		if v, err := evalBinaryExpr(be); err == nil {
 			fmt.Println(v)
 		} else {
 			fmt.Println("Error:", err)
