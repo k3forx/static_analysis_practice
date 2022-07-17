9d42a85e62ce1f46d43f3bf7dced3224366a6a93
Kanata Miyahana <afiri.bupa@gmail.com>
2022-07-17 23:12:40 +0900

    add more test cases

--- arg_checker/_example.go
+++ arg_checker/_example.go
@@ -9,4 +9,6 @@ func OKFunc1(ctx context.Context, id int, email string) {}
 
 func OKFunc2(ctx ctxPkg.Context, attrs map[string]interface{}) {}
 
+func OKFunc3(id int, email string, ctx context.Context) {}
+
 func InvalidFunc(c context.Context) {}
