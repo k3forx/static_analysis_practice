626424555a014ed83044df38bf9ad93d71f73ec7
Kanata Miyahana <afiri.bupa@gmail.com>
2022-07-17 23:30:54 +0900

    remove some comments and refactor

--- arg_checker/main.go
+++ arg_checker/main.go
@@ -18,19 +18,14 @@ func main() {
 		log.Fatal("Error: ", err)
 	}
 
-	// 識別子が定義または利用されてる部分を記録する
-	defsOrUses := map[*ast.Ident]types.Object{}
 	info := &types.Info{
-		Defs: defsOrUses,
-		Uses: defsOrUses,
+		Defs: map[*ast.Ident]types.Object{},
 	}
 
-	// 型チェックを行うための設定
 	config := &types.Config{
 		Importer: importer.Default(),
 	}
 
-	// 型チェックを行う
 	_, err = config.Check("main", fset, []*ast.File{file}, info)
 	if err != nil {
 		log.Fatal("Error:", err)
@@ -56,7 +51,6 @@ func main() {
 			// ast.Print(fset, p)
 			varIdent := p.Names[0]
 
-			// 識別子が定義または利用されている部分の情報を取得
 			obj := info.ObjectOf(varIdent)
 			if obj == nil {
 				return true
